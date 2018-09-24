c1a2c7ca1c859f377d06d95f370aec9ded6cd37c
Hanter Duarte <42872928+hanterduarte@users.noreply.github.com>
2018-09-24 13:42:36 -0300

    sessão 3 aula 14

---
+++ variaveis/exemplo-02.php
@@ -0,0 +1,23 @@
+<?php
+/*
+$anoNascimento = 1990;
+
+$nomeCompleto = "";*/
+
+//Na linha de baixo temos uma variável com número no nome
+
+$nome1 = "João";
+
+echo $nome1;
+
+echo "<br/>";
+
+//unset($nome1);
+
+if (isset($nome1)) {
+
+echo $nome1;
+
+}
+
+?>
